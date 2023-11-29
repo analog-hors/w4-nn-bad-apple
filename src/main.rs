2c07a7820ba3154d8dd923d3bc88c96ebdfc0fce
analog-hors <44050761+analog-hors@users.noreply.github.com>
2023-11-30 02:00:31 +1000

    Add code to allow linear interpolation in bad-apple-space

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 include!("../decoder_nn.rs");
 static FRAMES: &[u8] = include_bytes!("../encoded_frames.bin");
+const KEYFRAME_INTERVAL: usize = 1;
 
 fn linear<
     const I: usize,
@@ -31,8 +32,7 @@ fn sigmoid(n: f32) -> f32 {
     1.0 / (1.0 + (-n).exp())
 }
 
-fn decode(input: &[u8; FRAME_NUMS]) -> [u8; 20 * 20] {
-    let input = input.map(|n| n as f32 / 255.0);
+fn decode(input: &[f32; FRAME_NUMS]) -> [u8; 20 * 20] {
     let input = linear(&input, &WEIGHT_0, &BIAS_0);
     let input = input.map(mish);
     let input = linear(&input, &WEIGHT_2, &BIAS_2);
@@ -41,10 +41,31 @@ fn decode(input: &[u8; FRAME_NUMS]) -> [u8; 20 * 20] {
     input.map(|n| ((sigmoid(n) * 255.0).round() as i32).clamp(0, u8::MAX as i32) as u8)
 }
 
+static FRAME_COUNT: usize = FRAMES.len() / FRAME_NUMS;
+
+fn get_frame(i: usize) -> [f32; FRAME_NUMS] {
+    let i = i.clamp(0, FRAME_COUNT - 1);
+    let frame = &FRAMES[i * FRAME_NUMS..i * FRAME_NUMS + FRAME_NUMS];
+    let frame: [u8; FRAME_NUMS] = frame.try_into().unwrap();
+    frame.map(|n| n as f32 / 255.0)
+}
+
 fn main() {
-    for (i, frame) in FRAMES.chunks_exact(FRAME_NUMS).enumerate() {
-        let frame = frame.try_into().unwrap();
-        let frame = decode(frame);
+    for i in 0..FRAME_COUNT {
+        let frame = match i % KEYFRAME_INTERVAL {
+            0 => get_frame(i),
+            progress => {
+                let mut frame = get_frame(i - progress);
+                let target = get_frame(i - progress + KEYFRAME_INTERVAL);
+                for (f, t) in frame.iter_mut().zip(target) {
+                    let p = progress as f32 / KEYFRAME_INTERVAL as f32;
+                    *f = *f * (1.0 - p) + t * p;
+                }
+                frame
+            }
+        };
+
+        let frame = decode(&frame);
         let image = image::GrayImage::from_raw(20, 20, frame.to_vec()).unwrap();
         image.save(format!("decoded/{}.png", i + 1)).unwrap();
     }
